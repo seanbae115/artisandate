b31acae35b346f5b5ff1b831f8ce35071a573cb5
marc-gonzalez <marcgonzalez@outlook.com>
2018-02-13 14:33:39 -0800

    included js and css files for event page, did not import app.js or nav.js

---
+++ client/src/components/title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/css/title.css';
+
+function Title(props){
+    return (
+        <div id="title">
+            <div className="row">
+                <div className="col s12">
+                    <h1 className = 'center-align lobster'>Lola Gaspar</h1>
+                 </div>
+            </div>
+        </div>
+    );
+}
+
+export default Title;
