3091b180487d37bf8a7eed36e1701052c46e0d7d
marc-gonzalez <marcgonzalez@outlook.com>
2018-02-13 14:55:09 -0800

    imported materialize to evenPage components

--- client/src/components/jumboImg.js
+++ client/src/components/jumboImg.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
 import lola from '../assets/images/lola.jpg';
 import '../assets/css/jumboImg.css';
 
