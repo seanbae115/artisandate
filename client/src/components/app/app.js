b405771545d61f8018ad69907319d9132ba70e58
marc-gonzalez <marcgonzalez@outlook.com>
2018-02-19 10:16:48 -0800

    basic routing complete

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,17 +1,24 @@
 import "materialize-css/dist/css/materialize.min.css";
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from '../home/home';
 import ResultsPage from '../results-page/resultsPage';
 import LocationPage from '../location-page/locationPage';
 import EventPage from '../event-page/eventPage';
 import SummaryPage from "../summary-page/summaryPage";
-import Environment from "../event-page/environment";
+
 
 
 const App = () => (
-    <div>
-        <Environment/>
-    </div>
+    <Router>
+        <div>
+            <Route exact path = '/' component = {Home}/>
+            <Route path = '/location-page' component = {LocationPage}/>
+            <Route path = '/results-page' component = {ResultsPage}/>
+            <Route path = '/event-page' component = {EventPage}/>
+            <Route path = '/summary-page' component = {SummaryPage}/>
+        </div>
+    </Router>
 );
 
 export default App;
