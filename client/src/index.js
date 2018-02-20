b9bf7fcd0d2216df7a3af1379abb38a0b0343d00
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-19 18:30:00 -0800

    initiliazed redux implementation on results page. Still needs to be added to dom and to additional pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import promise from "redux-promise";
+import rootReducer from "./reducers"
 
 import App from './components/app/app';
 
+const store = createStore(rootReducer, {}, applyMiddleware(promise));
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
