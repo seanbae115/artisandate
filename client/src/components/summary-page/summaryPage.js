98328d2e7e3c58d090b7c2dfef5288ab09342c38
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-26 18:28:05 -0800

    finished dynamic marker placement

--- client/src/components/summary-page/summaryPage.js
+++ client/src/components/summary-page/summaryPage.js
@@ -10,13 +10,14 @@ import {MapComponent} from './map';
 class Summary extends Component{
     render() {
         console.log("Summary Page Props: ", this.props);
+        console.log('Latitude: ', this.props.event.coordinates.latitude);
         return (
             <div>
                 <NavBar/>
                 <SummaryEvent eventType="Event" eventName={this.props.event.name}/>
                 <SummaryEvent eventType="Food" eventName={this.props.food.name}/>
                 <SummaryEvent eventType="Drinks" eventName={this.props.drinks.name}/>
-                <MapComponent/>
+                <MapComponent eventLoc={this.props.event.coordinates} foodLoc={this.props.food.coordinates} drinkLoc={this.props.drinks.coordinates} />
                 <SummaryButtons/>
             </div>
         );
