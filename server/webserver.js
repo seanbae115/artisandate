2ddca73506f5efb762f99a7865363b3a3666cae9
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-02-27 14:36:30 -0800

    changed title to artisandate and updated route

--- server/webserver.js
+++ server/webserver.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '..','client','dist')));
 
+//bundle route
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
 });
