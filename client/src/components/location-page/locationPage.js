04f6171017ebaed016c715f13204b90a41cdd01a
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-28 10:34:17 -0800

    added valign-wrapper to properly align non-mobile versions

--- client/src/components/location-page/locationPage.js
+++ client/src/components/location-page/locationPage.js
@@ -12,7 +12,7 @@ class LocationPage extends Component {
     }
     renderInput(props){
         return (
-            <input {...props.input} placeholder = 'Zip Code' type = 'number' className='inputText center-align'/>
+            <input {...props.input}  type = 'number' className='inputText center-align'/>
         )
     }
     render(){
@@ -20,19 +20,21 @@ class LocationPage extends Component {
         return (
             <div className='grey lighten-4 location-page-container'>
                 <NavBar/>
-                <div className="row ">
-                    <div className="grey-text text-darken-3 date-location center-align">Let us know your date location to get started.</div>
-                    <form onSubmit = {this.props.handleSubmit(this.sendData.bind(this))}>
-                        <h3 className='card-heading zipText center-align'>Zip Code</h3>
-                        <div className='col s8 offset-s2'>
-                            <Field label = 'zip' name = 'zip' component = {this.renderInput}/>
-                        </div>
-                        <div className='row'>
-                            <div className= 'col s12 center-align'>
-                                <button className="btn-large blue go">Go</button>
+                <div className="valign-wrapper">
+                    <div className="row">
+                        <div className="grey-text text-darken-3 date-location center-align">Let us know your date location to get started.</div>
+                        <form onSubmit = {this.props.handleSubmit(this.sendData.bind(this))}>
+                            <h3 className='card-heading zipText center-align'>Zip Code</h3>
+                            <div className='col s8 offset-s2'>
+                                <Field label = 'zip' name = 'zip' component = {this.renderInput}/>
                             </div>
-                        </div>
-                    </form>
+                            <div className='row'>
+                                <div className= 'col s12 center-align'>
+                                    <button className="btn-large blue go">Go</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         )
