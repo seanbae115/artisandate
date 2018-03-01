37231b6db97d15f2f163ed5d9ff6f04aafdf20d9
ShaggyMunky <aaronguillen21@gmail.com>
2018-03-01 13:03:04 -0800

    completed retyling of the signup page

--- client/src/components/signup-page/signup.js
+++ client/src/components/signup-page/signup.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import { Link } from "react-router-dom";
-import axios from 'axios';
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { renderInput } from '../../helpers';
 import { signUp } from '../../actions';
+import "../../helpers/helper.css"
 
 class SignUpPage extends Component {
 
@@ -18,29 +17,31 @@ class SignUpPage extends Component {
     }
 
     render(){
-        var containerStyle = {
-            'height': '100vh',
+        const containerStyle = {
+            'height': '91.54vh',
             'width': '100vw'
         };
-        var h3Style = {
-            'marginTop': '0rem'
-        }
 
         return (
-            <div className="grey lighten-4" style={containerStyle}>
-                <div className="row">
-                    <form onSubmit={this.props.handleSubmit(this.handleSignUp.bind(this))}>
-                        <h3 className='center-align' style={h3Style}>Sign Up</h3>
-
-                        <div className='col s8 offset-s2'>
-                            <Field component={ renderInput } id='email' name='email' />
-                            <Field component={ renderInput } id='password' name='password' type='password' />
-                            <Field component={ renderInput } id='confirmPassword' name='confirmPassword' type='password' />
+            <div className="grey lighten-4 valign-wrapper" style={containerStyle}>
+                <div className="row card-width">
+                    <div className="col s10 offset-s1">
+                        <div className="card white">
+                            <div className="card-content">
+                                <span className='card-title center-align'>Sign Up</span>
+                                <form onSubmit={this.props.handleSubmit(this.handleSignUp.bind(this))}>
+                                    <Field component={ renderInput } id='email' name='email' />
+                                    <Field component={ renderInput } id='password' name='password' type='password' />
+                                    <Field component={ renderInput } id='confirmPassword' name='confirm password' type='password' />
+                                    <div className="row">
+                                        <div className='col s12 center-align'>
+                                            <button className='btn cyan' type='submit'>Sign Up</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <div className='col s12 center-align'>
-                            <button className='btn amber darken-3' type='submit'>Sign Up</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         )
@@ -51,10 +52,10 @@ function validate(values) {
     const error = {};
 
     if (!values.email) {
-        error.email = 'please enter an email'
+        error.email = 'Please enter an email'
     }
     if (!values.password) {
-        error.password = 'please choose a password'
+        error.password = 'Please choose a password'
     }
     if (values.password !== values.confirmPassword) {
         error.confirmPassword = 'Passwords do not match'
