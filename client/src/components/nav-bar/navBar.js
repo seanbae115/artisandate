2826e7bd592db07474d6d4715368d69615fb45df
ShaggyMunky <aaronguillen21@gmail.com>
2018-03-09 23:06:20 -0800

    corrected nav background to fade in, added our app link on desktop and fixed nav bar to always show

--- client/src/components/nav-bar/navBar.js
+++ client/src/components/nav-bar/navBar.js
@@ -8,12 +8,16 @@ class NavBar extends Component {
 
         this.state = {
             navStyle: {
-                transform: "translateX(100%)"
+                transform: "translateX(105%)"
             },
             dragStyle: {
-                backgroundColor: "rgba(0, 0, 0, 0)",
-                right: "0px",
-                touchAction: "pan-y"
+                backgroundColor: "#000",
+                width: "100%",
+                height: "100%",
+                touchAction: "pan-y",
+                visibility: "hidden",
+                opacity: "0",
+                transition: ".3s"
             }
         };
         this.slideOutMenu = this.slideOutMenu.bind(this);
@@ -21,32 +25,29 @@ class NavBar extends Component {
         this.backButton = this.backButton.bind(this);
     }
     slideOutMenu(){
-        const shadowBox = {...this.state.dragStyle};
-        delete shadowBox.right;
         this.setState({
             navStyle: {
                 transform: "translateX(0%)"
             },
             dragStyle: {
                 backgroundColor: "rgba(0, 0, 0, 0.5)",
-                left: "0px",
-                width: "50%",
-                transitionDuration: "0.25s"
+                visibility: "visible",
+                opacity: "1",
+                transition: ".3s"
             }
         });
     }
 
     returnMenu(){
-        const shadowBox = {...this.state.dragStyle};
-        delete shadowBox.left;
-        delete shadowBox.width;
         this.setState({
             navStyle: {
-                transform: "translateX(100%)"
+                transform: "translateX(105%)"
             },
             dragStyle: {
                 backgroundColor: "rgba(0, 0, 0, 0.0)",
-                right: "0px"
+                visibility: "hidden",
+                opacity: "0",
+                transition: ".3s"
             }
         });
     }
@@ -62,35 +63,38 @@ class NavBar extends Component {
     }
     render() {
         return (
-            <nav>
-                <div className="amber nav-wrapper">
-                    {this.backButton()}
-                    <a href="#!" className="brand-logo">Artisan Date</a>
-                    <a href="#" className="right button-collapse" onClick={this.slideOutMenu}>
-                        <i className="material-icons amber-text text-darken-3">menu</i>
-                    </a>
-                    <ul className="right hide-on-med-and-down">
-                        <li><Link to='/'>Home</Link></li>
-                        {/* <li><Link to='/location-page'>Get Started</Link></li> */}
-                        <li><Link to='/signup-page'>Sign Up</Link></li>
-                        <li><Link to='/signin-page'>Sign In</Link></li>
-                        <li><Link to='/ourteam-page'>Our Team</Link></li>
-                        {/* <li><Link to='/logout'>Log Out</Link></li> */}
-                    </ul>
-                    <div>
-                        <ul className="side-nav right-aligned" style={this.state.navStyle}>
-                            <li><Link to='/' onClick={this.returnMenu}>Home</Link></li>
+            <div className="navbar-fixed">
+                <nav>
+                    <div className="amber nav-wrapper">
+                        {this.backButton()}
+                        <a href="#!" className="brand-logo">Artisan Date</a>
+                        <a href="#" className="right button-collapse" onClick={this.slideOutMenu}>
+                            <i className="material-icons amber-text text-darken-3">menu</i>
+                        </a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><Link to='/'>Home</Link></li>
                             {/* <li><Link to='/location-page'>Get Started</Link></li> */}
-                            <li><Link to='/signup-page' onClick={this.returnMenu}>Sign Up</Link></li>
-                            <li><Link to='/signin-page' onClick={this.returnMenu}>Sign In</Link></li>
-                            <li><Link to='/ourapp-page' onClick={this.returnMenu}>Our App</Link></li>
-                            <li><Link to='/ourteam-page' onClick={this.returnMenu}>Our Team</Link></li>
+                            <li><Link to='/signup-page'>Sign Up</Link></li>
+                            <li><Link to='/signin-page'>Sign In</Link></li>
+                            <li><Link to='/ourapp-page'>Our App</Link></li>
+                            <li><Link to='/ourteam-page'>Our Team</Link></li>
                             {/* <li><Link to='/logout'>Log Out</Link></li> */}
                         </ul>
-                        <div onClick={this.returnMenu} className="drag-target" style={this.state.dragStyle}/>
+                        <div>
+                            <ul className="side-nav right-aligned" style={this.state.navStyle}>
+                                <li><Link to='/' onClick={this.returnMenu}>Home</Link></li>
+                                {/* <li><Link to='/location-page'>Get Started</Link></li> */}
+                                <li><Link to='/signup-page' onClick={this.returnMenu}>Sign Up</Link></li>
+                                <li><Link to='/signin-page' onClick={this.returnMenu}>Sign In</Link></li>
+                                <li><Link to='/ourapp-page' onClick={this.returnMenu}>Our App</Link></li>
+                                <li><Link to='/ourteam-page' onClick={this.returnMenu}>Our Team</Link></li>
+                                {/* <li><Link to='/logout'>Log Out</Link></li> */}
+                            </ul>
+                            <div onClick={this.returnMenu} className="drag-target" style={this.state.dragStyle}/>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         );
     }
 }
