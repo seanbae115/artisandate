37ea017a0d4c96a8b0128e8792e3cde7384cf0db
ShaggyMunky <aaronguillen21@gmail.com>
2018-03-22 14:27:52 -0700

    updating the view hight when in landscape on mobile for home, sign up and map screens

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -9,34 +9,18 @@ export default class Home extends Component {
         this.state = {
 
         }
-        // this.handleGetStarted = this.handleGetStarted.bind(this);
     }
 
     render(){
         return (
             <div className="full-page">
                 <div className="splash-image"/>
-                <div className="gradient valign-wrapper">
-                    <div className="row centerPage">
-                        <div className="col s-12 center-align">
-                            <div className="grey-text text-darken-3"><h2>Planning a date is hard.</h2></div>
-                            <div className="white-text need-ideas"><h3>Need ideas?<br/>We've got you covered.</h3></div>
-                            <div className="row">
-                                <div className="col s12 center-align">
-                                    <Link to='/signup-page' className="btn home-btn">Sign Up</Link>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col s12 center-align">
-                                    <Link to='/signin-page' className="btn home-btn">Sign In</Link>
-                                </div>
-                            </div>
-                            {/* <div className="row">
-                                <div className="col s12 center-align">
-                                        <Link to = '/location-page' className="btn home-btn">Get Started</Link>
-                                </div>
-                            </div> */}
-                        </div>
+                <div className="gradient valign-wrapper center-align">
+                    <div className="center-page">
+                        <h2 className="grey-text text-darken-3">Planning a date is hard.</h2>
+                        <h3 className="white-text">Need ideas?<br/>We've got you covered.</h3>
+                        <Link to='/signup-page' className="btn home-btn">Sign Up</Link>
+                        <Link to='/signin-page' className="btn home-btn">Sign In</Link>
                     </div>
                 </div>
             </div>
