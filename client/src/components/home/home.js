0aa3b70e54d033187c6181d78a459e7a59e60add
ShaggyMunky <aaronguillen21@gmail.com>
2018-03-22 14:57:54 -0700

    changed the order of element render for home screen to resolve document flow issue.

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -15,13 +15,12 @@ export default class Home extends Component {
         return (
             <div className="full-page">
                 <div className="splash-image"/>
-                <div className="gradient valign-wrapper center-align">
-                    <div className="center-page">
-                        <h2 className="grey-text text-darken-3">Planning a date is hard.</h2>
-                        <h3 className="white-text">Need ideas?<br/>We've got you covered.</h3>
-                        <Link to='/signup-page' className="btn home-btn">Sign Up</Link>
-                        <Link to='/signin-page' className="btn home-btn">Sign In</Link>
-                    </div>
+                <div className="gradient"/>
+                <div className="center-page center-align">
+                    <h2 className="grey-text text-darken-3">Planning a date is hard.</h2>
+                    <h3 className="white-text">Need ideas?<br/>We've got you covered.</h3>
+                    <Link to='/signup-page' className="btn home-btn">Sign Up</Link>
+                    <Link to='/signin-page' className="btn home-btn">Sign In</Link>
                 </div>
             </div>
         )
