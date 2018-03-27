363627d3cdf597097f825453fc33142e7f856ed0
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-03-26 20:47:16 -0700

    added favicon serving npm and app.use

--- server/webserver.js
+++ server/webserver.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const favicon = require('serve-favicon')
 const passport = require('passport');
 
 const { secret } = require('./config/credentials');
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 8000;
 
 //middleware
 app.use(cors());
+app.use(favicon(path.join(__dirname, '..', 'client', 'src', 'assets', 'images', 'Artisanfav.ico')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '..','client','dist')));
@@ -28,5 +30,5 @@ require('./routes/index.js')(app, path);
 require('./nodemailer/mail.js')(app, path);
 
 app.listen(PORT, ()=>{
-    console.log('the system is running on port', PORT)
+    console.log('the system is down on port', PORT)
 });
